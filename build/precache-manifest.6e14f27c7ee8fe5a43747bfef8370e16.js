a95db267a02bb6cd0c8cc28497722a16a8463956
Pac <paskalk@gmail.com>
2020-05-02 16:52:24 +0200

    forgot compiled files

---
+++ build/precache-manifest.6e14f27c7ee8fe5a43747bfef8370e16.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8e2e57ba63c4e234f221c1e4ea07618",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b21922e7e111c47103e2",
+    "url": "/static/css/2.6010d4dc.chunk.css"
+  },
+  {
+    "revision": "fc3396c04873073ab0ff",
+    "url": "/static/css/main.d3fe4292.chunk.css"
+  },
+  {
+    "revision": "b21922e7e111c47103e2",
+    "url": "/static/js/2.69ed94c1.chunk.js"
+  },
+  {
+    "revision": "5a2f267439a4f7e16cadb55d433cda3e",
+    "url": "/static/js/2.69ed94c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc3396c04873073ab0ff",
+    "url": "/static/js/main.b805f474.chunk.js"
+  },
+  {
+    "revision": "0151d698b2ab1237e12f",
+    "url": "/static/js/runtime-main.d63ec8cf.js"
+  },
+  {
+    "revision": "3d432ca2badb7e0130b379f9162b99b1",
+    "url": "/static/media/logo.3d432ca2.svg"
+  },
+  {
+    "revision": "8f30b68950ce7229e4f1f1f9ba9f2763",
+    "url": "/static/media/molecule.8f30b689.svg"
+  }
+]);
